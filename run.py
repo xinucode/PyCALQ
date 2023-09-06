f27a0f694d7b08937e6d231cbaef58dff50a63eb
Sarah Skinner <skinnersarah93@gmail.com>
2023-09-06 13:02:06 -0400

    general format idea by sarah

---
+++ run.py
@@ -0,0 +1,7 @@
+# this is the script that runs everything else
+
+# this script will ideally run one or some of the subset of tasks:
+#  - compute the fv spectrum (Sarah, Arjun)
+#  - use the luscher formalism to compute observables (Joseph)
+
+# we will hopefully break this down and detail out these steps further in the future
