2cd9299f0cab84fff517f8a85498b372ededcdbd
Sarah Skinner <skinnersarah93@gmail.com>
2023-09-27 23:32:49 -0400

    finished testing config handler as it is now

--- general/config_handler.py
+++ general/config_handler.py
@@ -13,14 +13,14 @@ class ConfigHandler:
     def init( self, input ):
         if type(input)==str:
             yaml_file = False; json_file = False; xml_file = False
-            try:
+            try: #tested
                 with open(input, 'r') as f:
                     config_info = yaml.safe_load(f)
                     yaml_file = True
             except:
                 logging.warning(f"{input} is not a YAML file")
                 
-            if not yaml_file:
+            if not yaml_file: #tested
                 try:
                     with open(input, 'r') as f:
                         config_info = json.load(f)
@@ -28,11 +28,17 @@ class ConfigHandler:
                 except:
                     logging.warning(f"{input} is not a JSON file")
                     
-            if not yaml_file and not json_file:
+            if not yaml_file and not json_file: #tested
                 try:
                     with open(input, 'r') as f:
-                        config_info = json.load(f)
+                        config_info = xmltodict.parse(f.read())
                         xml_file = True
+                        
+                    #first level guaranteed to only have one index
+                    config_info[list(config_info.keys())[0]] = dict(config_info[list(config_info.keys())[0]])
+                    
+                    #issue with xml everything that is input is interpreted as a string. need to do more input processing if taken seriously
+                    
                 except:
                     logging.warning(f"{input} is not a XML file")
                     
--- luscher_schmuscher.py
+++ luscher_schmuscher.py
@@ -1,8 +1,10 @@
+import general.config_handler as ch
+
 class LuscherSchmuscher:
 
     def __init__( self, general_configs, task_configs = {} ):
-        self.general_configs = general_configs
-        self.task_configs = task_configs
+        self.general_configs = ch.ConfigHandler(general_configs).configs
+        self.task_configs = ch.ConfigHandler(task_configs).configs
         
         #reformat configs/import them from files, combine task dicts if multiple
         
