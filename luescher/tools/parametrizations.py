333ad495e7c9837f3666ee58ad3887fef3e8a6aa
jomo101 <jomo101@email.unc.edu>
2024-08-11 21:45:22 -0400

    Changing ERE param to generalize

--- luescher/tools/parametrizations.py
+++ luescher/tools/parametrizations.py
@@ -19,3 +19,14 @@ def ere_delta(ecm,ma,mb,a,b):
 def ere(ecm,ma,mb,a,b):
     return ((-1/a)+0.5*b*kin_tools.q2(ecm,ma,mb) ) #in units of reference mass, usually mpi
 
+# define ere for range of n
+def ere(ecm,ma,mb,*p):
+    #qcotd = ere = p[0] + p[1] * x + ...
+    # p[0]= -1/a , ....
+    # x is q2
+    qcotd = 0
+    x = kin_tools.q2(ecm,ma,mb)
+    for n in range(len(p)):
+        qcotd += p[n] * x**n 
+    return qcotd 
+
